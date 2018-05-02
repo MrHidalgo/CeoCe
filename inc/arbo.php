f4e6cfde3e172501184620f2342b4809676bd2ce
MrHidalgo <Luckyhidalgo@gmail.com>
2018-05-02 17:15:44 +0300

    Initial commit

---
+++ inc/arbo.php
@@ -0,0 +1,15 @@
+<?php
+
+$pages = [
+	'0000' => [
+		'canonical' 	=> '',
+		'breadcrumb'	=> 'Homepage'
+	],
+	'2100' => [
+		'canonical' 	=> 'page-one',
+		'breadcrumb'	=> 'Page One'
+	],
+];
+
+
+?>
